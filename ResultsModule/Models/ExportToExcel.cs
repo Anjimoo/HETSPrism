b2a01689459169132b34d2741cf511a4264e9424
הוד נחום <hodnahum@gmail.com>
2020-08-25 22:08:48 +0300

    fix the export file

--- ResultsModule/Models/ExportToExcel.cs
+++ ResultsModule/Models/ExportToExcel.cs
@@ -10,18 +10,21 @@ namespace ResultsModule.Models
      public class ExportToExcel
     { 
        public static void ToCsv(ObservableCollection<HomeExercise> homeExercises)
-        {
+        {   // the path to folder project
             string AppPath = Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
             DirectoryInfo myDir = new DirectoryInfo(AppPath);
             string dataDir = myDir.Parent.Parent.FullName.ToString();
-           
+            string csvFiles = $"{dataDir}\\HomeExerciseReport.csv";
+
+            // create a table and export to csv file
             StringBuilder csvcontent = new StringBuilder();
             csvcontent.AppendLine("homeExercisePath,homeExerciseName,compilationOutput,compilationErrorOutput");
             foreach (var homeExercise in homeExercises)
             {
                 csvcontent.AppendLine($"{homeExercise.HomeExercisePath}, {homeExercise.HomeExerciseName}, {homeExercise.CompilationOutput},{homeExercise.CompilationErrorOutput}");      
             }
-            File.AppendAllText(dataDir, csvcontent.ToString());
+          
+             File.AppendAllText(csvFiles, csvcontent.ToString());
         }
 
 
